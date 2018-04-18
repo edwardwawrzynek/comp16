04c75962ac3379d768f085f308b9ead862d91e97
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-17 19:47:48 -0600

    Added ps2 interface, misc changes

--- IO/vga/vgaText.v
+++ IO/vga/vgaText.v
@@ -1,4 +1,4 @@
-module vgaTextGen(disp_en, x, y, pixel_clk, out, ram_clk, addr, data_in, data_out, we, text_mem_clk);
+module vgaTextGen(disp_en, x, y, pixel_clk, out, ram_clk, addr, data_in, data_out, we, text_mem_clk, col_out);
 
 input text_mem_clk;
 input pixel_clk;
@@ -6,21 +6,20 @@ input [31:0] x;
 input [31:0] y;
 input disp_en;
 
-output reg out;
-
 //For writing to text ram
 input ram_clk;
 input [9:0] addr;
-output [7:0] data_out;
-input [7:0] data_in;
+output [15:0] data_out;
+input [15:0] data_in;
 reg [9:0] text_addr;
 input we;
 
+output reg out;
 /*Memory Initilization for charsets and text ram*/
 parameter CHARSET_MEM_FILE = "charset.txt";
 
 reg [63:0] charset_ram [0:255];
-reg [7:0] text_mem [0:999];
+reg [15:0] text_mem [0:999];
 
 wire [7:0] charset_index;
 wire [63:0] charset;
@@ -45,11 +44,13 @@ assign xChar = x[9:4];
 wire [5:0] yChar;
 assign yChar = y[9:4];
 
-
+//Non-color
 assign text_index = xChar + (10'd40 * yChar);
 
 reg [10:0] real_text_index;
-assign charset_index = text_mem[real_text_index];
+wire [15:0] current_char;
+assign current_char = text_mem[real_text_index];
+assign charset_index = current_char[7:0];
 
 assign charset = charset_ram[charset_index];
 
@@ -57,10 +58,19 @@ wire [5:0] charset_pixel;
 assign charset_pixel = (charPixelY << 6'd3) + charPixelX;
 
 wire pixel;
-assign pixel = disp_en ? charset[6'd63 - charset_pixel] : 8'b0;
+assign pixel = disp_en ? charset[6'd63 - charset_pixel] : 1'b0;
 
+//Col
+wire [7:0] col;
+output reg [7:0] col_out;
+wire [7:0] mem_col;
+assign mem_col = current_char[15:8];
+assign col = pixel ? (mem_col == 8'b0 ? 8'd255 : mem_col) : 8'b0;
+
+//CLocked Memory and signal stabalization
 always @ (posedge pixel_clk)
 begin
+	col_out <= col;
 	out <= pixel;
 end
 
--- output_files/comp16.cdf
+++ output_files/comp16.cdf
@@ -6,7 +6,7 @@ JedecChain;
 	P ActionCode(Ign)
 		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
 	P ActionCode(Cfg)
-		Device PartName(5CSEMA4U23) Path("/home/edward/comp16/output_files/") File("comp16.sof") MfrSpec(OpMask(1));
+		Device PartName(5CSEMA4) Path("/home/edward/comp16/output_files/") File("comp16_serial_bootloader.jic") MfrSpec(OpMask(1) SEC_Device(EPCS128) Child_OpMask(1 1));
 
 ChainEnd;
 
